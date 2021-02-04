408c86586a4adb1bedde773261e7aac96faf839f
Antoine <antoine.lorin2@gmail.com>
2021-02-04 23:40:52 +0100

    Final Project

--- main.c
+++ main.c
@@ -18,13 +18,74 @@ void trim(char * str);
 void askInput ();
 void flushTab (char * params[], int size);
 int checkPipe(char * params[], int size);
+void piper(int pipeVar[2], char * command[10][20], int size, int it);
+void cleanCommand();
 
 char saisie[2048];
 
 // Tableau de chaîne de caractères pour stocker la suite de token
-char * elements[32];
-char * piped[2][32];
+char * elements[64];
+char * command[5][20];
 
+void piper(int pipeVar[2], char * command[10][20], int size, int it) {
+    int inner_pipe[2];
+    if (it == size-1) {
+
+        close(STDIN_FILENO);   //closing stdin
+        dup(pipeVar[0]);         //replacing stdin with pipe read
+        close(pipeVar[1]);       //closing pipe write
+        close(pipeVar[0]);
+
+        execvp(command[it][0], command[it]);
+        exit(1);
+    } else if (it == 0) {
+        pipe(inner_pipe);
+        pid_t pid = fork();
+
+        if (pid == 0) {
+            close(STDOUT_FILENO);  //closing stdout
+            dup(inner_pipe[1]);         //replacing stdout with pipe write
+            close(inner_pipe[0]);       //closing pipe read
+            close(inner_pipe[1]);
+
+            execvp(command[it][0], command[it]);
+            exit(1);
+        } else {
+            close(pipeVar[0]);
+            close(pipeVar[1]);
+            it++;
+            piper (inner_pipe, command, size, it);
+            exit(1);
+        }
+
+    } else {
+        pid_t pid;
+        pipe(inner_pipe);
+        pid = fork();
+        if (pid == 0) {
+
+            close(STDOUT_FILENO);  //closing stdout
+            close(STDIN_FILENO);  //closing stdin
+            dup(pipeVar[0]);
+            close(pipeVar[1]);       //closing write read
+            close(pipeVar[0]);
+
+            dup(inner_pipe[1]);         //replacing stdout with pipe write
+            close(inner_pipe[0]);       //closing pipe read
+            close(inner_pipe[1]);
+
+            execvp(command[it][0], command[it]);
+            exit(1);
+        } else {
+            close(pipeVar[0]);
+            close(pipeVar[1]);
+            it++;
+            piper(inner_pipe, command, size, it);
+            exit(1);
+        }
+    }
+
+}
 
 void trim(char * str) {
     if (str[strlen(str)-1] == '\n')
@@ -47,13 +108,6 @@ int checkPipe(char * params[], int size) {
     return pipeFlag;
 }
 
-void updateElements(int index) {
-    for (int i = 0; i< 32; i++) {
-        if (i < index) piped[0][i] = elements[i] ;
-        if (i > index) piped[1][i-(index + 1)] = elements[i] ;
-    }
-}
-
 void flushTab (char * params[], int size) {
     for (int i = 0; i < size; i++) {
         params[i] = 0;
@@ -61,7 +115,7 @@ void flushTab (char * params[], int size) {
 }
 
 int waitInput () {
-    flushTab(elements, 32);
+    flushTab(elements, 64);
     fgets(saisie, 2048, stdin);
     fflush(stdin);
 
@@ -82,60 +136,59 @@ int waitInput () {
     return 0;
 }
 
+int createCommand() {
+    int cmd_idx = 0;
+    int pipe_index = 0;
+    for (int i = 0; i < 64;i++) {
+        if (elements[i] == NULL) break;
+        if (elements[i][0] == '|') {
+            cmd_idx++;
+            pipe_index = i;
+            continue;
+        }
+        command[cmd_idx][i-(pipe_index + cmd_idx)] = elements[i];
+    }
+
+    return cmd_idx;
+}
+
+void cleanCommand() {
+    for (int i = 0; i < 5;i++) {
+        for (int y = 0; y< 20;y++) {
+            command[i][y] = 0;
+        }
+    }
+}
+
 void execProg() {
     int pipe_int[2], index;
     pid_t pid, pidPipe;
 
-    pid = fork();
+    int size = createCommand();
 
-    index = checkPipe(elements, 32);
+    if (size != 0) {
+        pipe(pipe_int);
+        pid = fork();
 
-    if(pid < 0)
-    {
-        return;
-    } else if (pid == 0) {
-        if (index != -1) {
-            pipe(pipe_int);
-            updateElements(index);
-            pidPipe = fork();
-            if (pidPipe == 0) {
-                close(STDOUT_FILENO);  //closing stdout
-                dup(pipe_int[1]);         //replacing stdout with pipe write
-                close(pipe_int[0]);       //closing pipe read
-                close(pipe_int[1]);
-
-                int ret = execvp(piped[0][0],piped[0]);
-                perror("Error Executing cmd");
-                if (ret == -1) printf("Unknown command :( \n");
-                exit(1);
-            } else {
-                close(STDIN_FILENO);   //closing stdin
-                dup(pipe_int[0]);         //replacing stdin with pipe read
-                close(pipe_int[1]);       //closing pipe write
-                close(pipe_int[0]);
-
-                int ret = execvp(piped[1][0],piped[1]);
-                perror("Error Executing cmd");
-                if (ret == -1) printf("Unknown command :( \n");
-                exit(1);
-            }
-        } else {
-            int ret = execvp(elements[0],elements);
-            perror("Error Executing cmd");
-            if (ret == -1) printf("Unknown command :( \n");
-        }
-        exit(1);
-    } else {
-        if (index != -1) {
-            close(pipe_int[0]);
-            close(pipe_int[1]);
-            wait(0);
-            wait(0);
+        if (pid == 0) {
+            piper(pipe_int, command, size+1, 0);
+            exit(1);
         } else {
             int status;
             wait(&status);
         }
 
+    } else {
+        pid = fork();
+
+        if (pid == 0) {
+            int ret = execvp(elements[0],elements);
+            perror("Error Executing cmd");
+            if (ret == -1) printf("Unknown command :( \n");
+        } else {
+            int status;
+            wait(&status);
+        }
     }
 }
 
@@ -147,8 +200,9 @@ int main(void) {
         input = waitInput();
         if(input == 1) break;
         if (input == 2) continue;
+        cleanCommand();
         execProg();
     }
-    printf("Bye :(");
+    printf("Bye :( \n");
     return EXIT_SUCCESS;
 }
